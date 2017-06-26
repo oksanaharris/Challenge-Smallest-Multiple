91d7ce04f33ed60c01099c1c3f07bd6baf83df0b
Oksana Harr <oksana.harris@gmail.com>
2017-06-26 10:11:40 -1000

    working

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 /**
  * Build a function that finds the smallest positive number that is evenly
  * divisible by all of the numbers starting from 1 up to the value passed into your function.
@@ -9,6 +10,32 @@
 module.exports = function( ceiling ) {
   // do work here
 
+  var arr = [];
+  for (let i = ceiling; i > 0; i--){
+    arr.push(i);
+  }
 
-  return 0;
+  var startProduct = 1;
+  for (let i = arr.length - 1; i > arr.length - 5; i-- ){
+    startProduct *= i;
+  }
+
+  var smallestMultiple = startProduct;
+  var passed = false;
+    
+  for (let j = startProduct; j > 0; j--){
+    for (let k = 0; k < arr.length; k++){
+      if (j % Number(arr[k]) === 0){
+        passed = true;
+      } else {
+        passed = false;
+        break;
+      }
+    }
+    if (passed === true){
+      smallestMultiple = j;
+    }
+  }
+  
+  return smallestMultiple;
 };
